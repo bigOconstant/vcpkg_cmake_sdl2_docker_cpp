fe07c15436fd73ec047a0edbfed5a2402562cb27
caleb <caleb.a.mccarthy@gmail.com>
2020-12-27 23:19:25 -0600

    Working sdl2 window appearing and disappearing

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,10 @@ RUN apt-get install curl -y
 RUN apt-get install zip -y
 RUN apt-get install gdb -y
 RUN apt-get install pkg-config -y
+
+RUN apt-get install libsdl2-dev -y
+RUN apt-get install libsdl2-ttf-dev -y
+RUN apt-get install libsdl2-image-dev -y
 #install vcpkg package manager
 RUN git clone https://github.com/microsoft/vcpkg
 RUN ./vcpkg/bootstrap-vcpkg.sh
@@ -18,6 +22,11 @@ RUN ./vcpkg/bootstrap-vcpkg.sh
 #install packages for project
 RUN /vcpkg/vcpkg install sqlite3
 RUN /vcpkg/vcpkg install nlohmann-json
+RUN /vcpkg/vcpkg install sdl2
+RUN /vcpkg/vcpkg install sdl2pp
+RUN /vcpkg/vcpkg install sdl2-mixer
+RUN /vcpkg/vcpkg install sdl2-ttf
+RUN /vcpkg/vcpkg install sdl2-image
 
 # set work directory for project
 WORKDIR /Project
--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <sqlite3.h>
 #include <iostream>
 #include <nlohmann/json.hpp>
-
+#include <exception>
+#include <SDL2pp/SDL2pp.hh>
 using std::cout;
 using std::endl;
 
+using namespace SDL2pp;
 /* Build manually with,
 mkdir build
 cd build
@@ -13,11 +15,40 @@ cmake ..
 make
  */
 
-int main()
-{
+int main() try {
+
     cout<<"starting app"<<endl;
     cout<< sqlite3_libversion()<<endl;
 
     nlohmann::json j;
-    return 0;
+   SDL sdl(SDL_INIT_VIDEO);
+
+	// Create main window: 640x480 dimensions, resizable, "SDL2pp demo" title
+	Window window("SDL2pp demo",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			640, 480,
+			SDL_WINDOW_RESIZABLE);
+
+	// Create accelerated video renderer with default driver
+	Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	// Load sprites image as a new texture
+
+	// Clear screen
+	renderer.Clear();
+
+
+
+	// Show rendered frame
+	renderer.Present();
+
+	// 5 second delay
+	SDL_Delay(5000);
+
+	// Here all resources are automatically released and library deinitialized
+	return 0;
+} catch (std::exception& e) {
+	// If case of error, print it and exit with error
+	std::cerr << e.what() << std::endl;
+	return 1;
 }
