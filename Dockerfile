d8aceb15ca8f442a1e1099dc2f7928ef35879f7d
caleb <caleb.a.mccarthy@gmail.com>
2020-12-27 21:02:01 -0600

    Add json parser as an example

--- Dockerfile
+++ Dockerfile
@@ -10,13 +10,14 @@ RUN apt-get install git -y
 RUN apt-get install curl -y
 RUN apt-get install zip -y
 RUN apt-get install gdb -y
-
+RUN apt-get install pkg-config -y
 #install vcpkg package manager
 RUN git clone https://github.com/microsoft/vcpkg
 RUN ./vcpkg/bootstrap-vcpkg.sh
 
 #install packages for project
 RUN /vcpkg/vcpkg install sqlite3
+RUN /vcpkg/vcpkg install nlohmann-json
 
 # set work directory for project
 WORKDIR /Project
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // main.cpp
 #include <sqlite3.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 using std::cout;
 using std::endl;
@@ -8,7 +9,7 @@ using std::endl;
 /* Build manually with,
 mkdir build
 cd build
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake ..
 make
  */
 
@@ -16,5 +17,7 @@ int main()
 {
     cout<<"starting app"<<endl;
     cout<< sqlite3_libversion()<<endl;
+
+    nlohmann::json j;
     return 0;
 }
